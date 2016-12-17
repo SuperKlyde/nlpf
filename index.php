f1b0c952fc92d7e2737e2277580751cb149a6fb4
SuperKlyde <hugo.capes@epita.fr>
2016-12-17 15:42:55 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+phpinfo();/*
+// index.php                                                                                                                                                                                                                     
+$link = new PDO("mysql:host=localhost;dbname=blog", 'root', 'root');
+$result = $link->query('SELECT id, title FROM post');
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>List of Posts - Jour 1</title>
+  </head>
+  <body>
+    <h1>List of Posts</h1>
+    <ul>
+      <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
+       <li>
+        <a href="/show.php?id=<?= $row['id'] ?>">
+          <?= $row['title'] ?>
+        </a>
+       </li>
+       <?php endwhile ?>
+    </ul>
+  </body>
+</html>
+<?php
+$link = null;
+*/?>

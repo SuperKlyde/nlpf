b9fd8f46b21d26f3110ea93a21df489785503113
SuperKlyde <hugo.capes@epita.fr>
2016-12-17 17:07:07 +0100

    [INIT] Changed index.php with hello world

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <?php
-phpinfo();/*
+//phpinfo();
+
+
+
+/*
 // index.php                                                                                                                                                                                                                     
 $link = new PDO("mysql:host=localhost;dbname=blog", 'root', 'root');
 $result = $link->query('SELECT id, title FROM post');
 ?>
-
+*/
 <!DOCTYPE html>
 <html>
   <head>
     <title>List of Posts - Jour 1</title>
   </head>
   <body>
-    <h1>List of Posts</h1>
-    <ul>
-      <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
-       <li>
-        <a href="/show.php?id=<?= $row['id'] ?>">
-          <?= $row['title'] ?>
-        </a>
-       </li>
-       <?php endwhile ?>
-    </ul>
+    <h1>Hello World!</h1>
   </body>
 </html>
-<?php
 $link = null;
-*/?>
+?>
